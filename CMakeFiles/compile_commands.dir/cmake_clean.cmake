5479ec9640c38a0207d6f6b47f6bf1bc4b5ad14f
Your Name <you@example.com>
2019-09-15 19:47:03 +0800

    first commit

---
+++ CMakeFiles/compile_commands.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compile_commands.dir/main.cpp.o"
+  "compile_commands.pdb"
+  "compile_commands"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compile_commands.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
